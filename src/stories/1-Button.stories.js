b6d2d96fd6a78aec5e49f9888152cc9980e5b65c
Philipp Schardt <philipp.schardt@ergosign.de>
2022-03-07 14:48:13 +0100

    updated to latest version, inlineGrid

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,44 +1,68 @@
-import React from 'react';
-import {AttributesStatesDefault, PseudoStatesDefault, withPseudo} from "@ergosign/storybook-addon-pseudo-states-react";
-import {SimpleButton} from "../SimpleButton/simple-button";
-import {SimpleButton__withoutCssModules} from "../SimpleButtonWithoutCssModules/simple-button";
-import {boolean} from "@storybook/addon-knobs";
+import React from "react";
+import {
+  AttributesStatesDefault,
+  PseudoStatesDefault,
+  Orientation,
+  withPseudo,
+} from "@ergosign/storybook-addon-pseudo-states-react";
+import { SimpleButton } from "../SimpleButton/simple-button";
+import { SimpleButton__withoutCssModules } from "../SimpleButtonWithoutCssModules/simple-button";
+import { boolean } from "@storybook/addon-knobs";
 
 export default {
-    title: 'Button',
-    decorators: [
-        withPseudo
-    ],
-    parameters: {
-        withPseudo: {
-            pseudos: [...PseudoStatesDefault, 'focus & hover'],
-            attributes: [...AttributesStatesDefault],
-            permutations: [{attr:'theme', value: 'dark'}, 'isBig']
-        }
-    }
+  title: "Button",
+  decorators: [withPseudo],
+  parameters: {
+    withPseudo: {
+      pseudos: [...PseudoStatesDefault, "focus & hover"],
+      attributes: [...AttributesStatesDefault],
+      //   permutations: [{ attr: "theme", value: "dark" }, "isBig"],
+    },
+  },
 };
 
-export const simpleButton = () => <SimpleButton label={'ButtonLabel'} rounded={boolean('rounded', false)} />;
+export const simpleButton = () => (
+  <SimpleButton label={"ButtonLabel"} rounded={boolean("rounded", false)} />
+);
 
-export const simpleButtonPermutation = () => <SimpleButton label={'ButtonLabel'} rounded={boolean('rounded', false)} />;
+export const simpleButtonPermutation = () => (
+  <SimpleButton label={"ButtonLabel"} rounded={boolean("rounded", false)} />
+);
 simpleButtonPermutation.parameters = {
-    withPseudo: {
-        attributes: [...AttributesStatesDefault],
-        permutations: [{attr:'theme', label: 'Dark Theme', value: 'dark'}, {attr:'theme', label: 'Light Theme', value: 'light'}, 'isBig']
-    }
-}
+  withPseudo: {
+    attributes: [...AttributesStatesDefault],
+    permutations: [
+      { attr: "theme", label: "Dark Theme", value: "dark" },
+      { attr: "theme", label: "Light Theme", value: "light" },
+      "isBig",
+    ],
+    styles: {
+      inlineGrid: true,
+    },
+  },
+};
 
-export const simpleButtonWithoutAttr = () => <SimpleButton label={'ButtonLabel'} rounded={boolean('rounded', false)} />;
+export const simpleButtonWithoutAttr = () => (
+  <SimpleButton label={"ButtonLabel"} rounded={boolean("rounded", false)} />
+);
 simpleButtonWithoutAttr.parameters = {
-    withPseudo: {
-        pseudos: [],
-        permutations: [{attr:'theme', label: 'Dark Theme', value: 'dark'}, {attr:'theme', label: 'Light Theme', value: 'light'}, 'isBig']
-    }
-}
+  withPseudo: {
+    pseudos: [],
+    permutations: [
+      { attr: "theme", label: "Dark Theme", value: "dark" },
+      { attr: "theme", label: "Light Theme", value: "light" },
+      "isBig",
+    ],
+    styles: {
+      orientation: Orientation.ROW,
+      inlineGrid: true,
+    },
+  },
+};
 
-
-
-export const simpleButtonWithoutCssModules = () => <SimpleButton__withoutCssModules label={'ButtonLabel'}/>;
+export const simpleButtonWithoutCssModules = () => (
+  <SimpleButton__withoutCssModules label={"ButtonLabel"} />
+);
 
 // export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
 //
